60f389788f7cab602a5e99fd0250f5e4e2daa804
Mario Magomedov <webdevelope2017@gmail.com>
2017-12-10 12:32:17 +0300

    add scope in js for homepage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,21 +6,24 @@ let login = document.querySelector('.login__btn');
 let authWrap = document.querySelector('#auth__wrap');
 let authInp = document.querySelector('.auth__box-bg');
 
-if(login != undefined){
-    login.onclick = function(e){
-        e.preventDefault();
-        
-        document.querySelector('#auth__wrap').classList.toggle('dhover');
+function authRotateHome() {
+    if(login != undefined){
+        login.onclick = function(e){
+            e.preventDefault();
+            
+            document.querySelector('#auth__wrap').classList.toggle('dhover');
 
-        if(authWrap.className == 'dhover'){
-            document.querySelector('.auth__box').style.display = 'none';
-            authInp.style.opacity = 1;
-            authInp.style.display = 'block';
-        } else{
-            document.querySelector('.auth__box').style.display = 'block';
-            authInp.style.opacity = 0;
-            authInp.style.display = 'none';
+            if(authWrap.className == 'dhover'){
+                document.querySelector('.auth__box').style.display = 'none';
+                authInp.style.opacity = 1;
+                authInp.style.display = 'block';
+            } else{
+                document.querySelector('.auth__box').style.display = 'block';
+                authInp.style.opacity = 0;
+                authInp.style.display = 'none';
+            }
         }
     }
 }
 
+authRotateHome();
